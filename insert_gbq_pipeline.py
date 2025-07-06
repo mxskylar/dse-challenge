49a4548a09bdc2efc5b45b5fb7c652e9aa46eba5
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2025-07-06 08:20:33 -0400

    Adds comment

--- insert_gbq_pipeline.py
+++ insert_gbq_pipeline.py
@@ -41,6 +41,7 @@ def insert_events(file_path: str, tries: int = 3):
             table = client.get_table("wfp-data-project.mobilize.events")
             client.insert_rows(table, rows)
             break
+        # Retries up to n times if the insert to GBQ times out
         except RetriableGbqTimeoutException as e:
             tries = tries - 1
             if tries == 0:
